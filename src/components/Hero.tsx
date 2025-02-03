359ff7b559715c35e8cdd26cd17bcf1ce4db568c
DEEPAK SHARMA <168280514+DeepakSharma104@users.noreply.github.com>
2025-02-02 20:13:02 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ export const Hero = () => {
           >
             <img
               src="me.jpg" // Replace "your-image.jpg" with your actual image filename
-              alt="Deepak Sharma"
+              alt="Aman sharam"
               className="w-40 h-40 md:w-48 md:h-48 rounded-full mx-auto shadow-xl object-cover border-4 border-accent"
             />
           </motion.div>
